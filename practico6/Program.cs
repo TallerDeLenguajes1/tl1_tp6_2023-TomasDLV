f065535cf7951c5d991a504b346089b4148cd802
TomasDLV <tomas01dlv@gmail.com>
2023-06-18 18:36:18 -0300

    Configurando entorno

---
+++ practico6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+float a;
+float b;
+a=10;
+b=a;
+string s = "108";
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+if (float.TryParse(s,out a))
+{
+    if (a > 0)
+    {
+        Console.WriteLine(a);
+        Console.WriteLine(1/a);    
+    }
+}
